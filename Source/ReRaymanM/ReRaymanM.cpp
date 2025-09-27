519b26163bf2af6d424af2f9ccb031a8c95c3328
rge15 <rge15.ua@gmail.com>
2025-09-27 17:23:19 +0100

    Initial Commit

---
+++ Source/ReRaymanM/ReRaymanM.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ReRaymanM.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ReRaymanM, "ReRaymanM" );
---
+++ Source/ReRaymanM/TP_ThirdPerson/TP_ThirdPerson.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ReRaymanM.h"
+#include "TP_ThirdPerson.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TP_ThirdPerson, "TP_ThirdPerson" );
+ 
